91b6d0c35f6e5b0126c762d9d676222efcb66f74
dhanushkumar-amk <dhanushkumaramk@gmail.com>
2024-03-17 23:40:30 +0530

    transpose matrix

---
+++ transpose.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+public class transpose
+{
+    public static void main(String args[])
+    {
+        int i,j;
+        int input[][] = new int[3][3];
+        int transpose[][] = new int[3][3];
+
+        Scanner s = new Scanner(System.in);
+ System.out.println("Enter the 3 * 3 matrix");
+ for(i = 1; i < 3; i++)
+ {
+    for(j =1; j < 3; j++)
+    {
+        input[i][j] = s.nextInt();
+    }
+ }
+
+ for(i = 1; i < 3; i++)
+ {
+    for(j =1; j < 3; j++)
+    {
+        transpose[i][j] =  input[j][i];
+    }
+ }
+
+ System.out.println("Transpose matrix:");
+
+
+ for(i = 1; i < 3; i++)
+ {
+    for(j =1; j < 3; j++)
+    {
+    System.out.print(transpose[i][j] + " ");
+    }
+    System.out.print("\n");
+ }
+
+
+    }
+}
